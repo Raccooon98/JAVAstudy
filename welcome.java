c2e37336199bbbd3db720fc9233fc31423c05507
Raccooon98 <redbull1171@gmail.com>
2022-09-05 14:43:54 +0900

    변수(1)

--- welcome.java
+++ welcome.java
@@ -1,5 +1,24 @@
-class es{
-    public static void main(String args[]){
+class ex2_1{
+    public static void main(String[] args){
         System.out.println("hello");
+        System.out.print("Hello");
+        System.out.println("World");
     }
 }
+
+class ex2_2{
+    public static void main(String[] args){
+        System.out.print("3+5= ");
+        System.out.println(3+5);
+    }
+}
+
+class ex2_3{
+    public static void main(String args[]){
+        System.out.println(5+3);
+        System.out.println(5-3);
+        System.out.println(5*3);
+        System.out.println(5/3);
+    }
+}
+
